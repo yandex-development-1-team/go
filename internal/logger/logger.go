15901844fd355b609994bff36257d546eff42e49
AlexFox86 <alek-fokin@yandex.ru>
2026-02-04 08:15:05 +0300

    add struct

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -5,6 +5,11 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+// Logger структура логгера
+type Logger struct {
+	Logger *zap.Logger
+}
+
 // setEnvironment определяет и устанавливает конфигурацию Development/Production
 func setEnvironment(mode string) zap.Config {
 	if mode == "dev" {
@@ -25,15 +30,44 @@ func setLevel(config *zap.Config, env string) {
 }
 
 // NewLogger создает и настраивает логгер
-func NewLogger(mode string, level string) *zap.Logger {
+func NewLogger(mode string, level string) *Logger {
 	config := setEnvironment(mode)
 	setLevel(&config, level)
 
 	logger, err := config.Build()
 	if err != nil {
 		fallback, _ := zap.NewProduction()
-		return fallback
+		return &Logger{
+			Logger: fallback,
+		}
 	}
 
-	return logger
+	return &Logger{
+		Logger: logger,
+	}
+}
+
+// Info логирование информационных сообщений
+func (l *Logger) Info(msg string, fields ...zap.Field) {
+	l.Logger.Info(msg, fields...)
+}
+
+// Error логирование ошибок
+func (l *Logger) Error(msg string, fields ...zap.Field) {
+	l.Logger.Error(msg, fields...)
+}
+
+// Warn логирование предупреждений
+func (l *Logger) Warn(msg string, fields ...zap.Field) {
+	l.Logger.Warn(msg, fields...)
+}
+
+// Debug логирование отладочной информации
+func (l *Logger) Debug(msg string, fields ...zap.Field) {
+	l.Logger.Debug(msg, fields...)
+}
+
+// Fatal логирование критических ошибок
+func (l *Logger) Fatal(msg string, fields ...zap.Field) {
+	l.Logger.Fatal(msg, fields...)
 }
